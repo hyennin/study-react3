cdcf39024f02af5875bbd2c4de7d258f9f94d04f
biyakim <w2116@e-mirim.hs.kr>
2023-02-26 22:41:15 +0900

    원래대로

--- src/App.js
+++ src/App.js
@@ -5,73 +5,73 @@ import { faBars, faUser, faTimes, faHeart } from "@fortawesome/free-solid-svg-ic
 import Yoga from "./yoga/yoga";
 import Condition from "./Condition";
 
-// const Header = styled.div`
-//   max-width: 1280px;
-//   margin: 0 auto;
-//   display: flex;
-//   justify-content: space-between;
-//   align-items: center;
-//   color: white;
-//   background-color: #558B2F;
-//   .logo {
-//     margin: 0 1rem;
-//     font-size: 2rem;
-//   }
-//   .header__menulist {
-//     list-style: none;
-//     display: flex;
-//   }
-//   .header__left {
-//     display: flex;
-//   }
-//   .header__right {
-//     list-style: none;
-//     display: flex;
-//   }
-//   .header__right div {
-//     margin: 0 1rem;
-//   }
-//   li {
-//     padding: 0 1rem;
-//     cursor: pointer;
-//   }
-//   .toggle {
-//     display: none;
-//     font-size: 1.5rem;
-//     padding: 1rem 1rem;
-//   }
-//   .user {
-//     display: none;
-//     font-size: 1.5rem;
-//     padding: 1rem 1rem;
-//   }
-//   @media screen and (max-width: 768px) {
-//     flex-wrap: wrap;
-//     .header__right {
-//       display: ${(props) => (props.userToggled ? "flex" : "none")};
-//       flex-direction: column;
-//       width: 100%;
-//       background-color: black;
-//     }
-//     .header__menulist {
-//       display: ${(props) => (props.isToggled ? "flex" : "none")};
-//       flex-direction: column;
-//       width: 100%;
-//       background-color: black;
-//     }
-//     .header__menulist li,
-//     .header__right li {
-//       margin: 1rem 0;
-//       padding: 0;
-//     }
-//     .toggle {
-//       display: block;
-//     }
-//     .user {
-//       display: block;
-//     }
-//   }
-// `;
+const Header = styled.div`
+  max-width: 1280px;
+  margin: 0 auto;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  color: white;
+  background-color: #558B2F;
+  .logo {
+    margin: 0 1rem;
+    font-size: 2rem;
+  }
+  .header__menulist {
+    list-style: none;
+    display: flex;
+  }
+  .header__left {
+    display: flex;
+  }
+  .header__right {
+    list-style: none;
+    display: flex;
+  }
+  .header__right div {
+    margin: 0 1rem;
+  }
+  li {
+    padding: 0 1rem;
+    cursor: pointer;
+  }
+  .toggle {
+    display: none;
+    font-size: 1.5rem;
+    padding: 1rem 1rem;
+  }
+  .user {
+    display: none;
+    font-size: 1.5rem;
+    padding: 1rem 1rem;
+  }
+  @media screen and (max-width: 768px) {
+    flex-wrap: wrap;
+    .header__right {
+      display: ${(props) => (props.userToggled ? "flex" : "none")};
+      flex-direction: column;
+      width: 100%;
+      background-color: black;
+    }
+    .header__menulist {
+      display: ${(props) => (props.isToggled ? "flex" : "none")};
+      flex-direction: column;
+      width: 100%;
+      background-color: black;
+    }
+    .header__menulist li,
+    .header__right li {
+      margin: 1rem 0;
+      padding: 0;
+    }
+    .toggle {
+      display: block;
+    }
+    .user {
+      display: block;
+    }
+  }
+`;
 
 function App() {
   const [isToggled, setIsToggled] = useState(false);
@@ -79,48 +79,46 @@ function App() {
   
   return (
     
-    // <Yoga/>  
-    <Condition/>
-    // <Header isToggled={isToggled} userToggled={userToggled}>
-    //   {/* 햄버거 버튼(bar) */}
-    //   <div
-    //     className="toggle"
-    //     onClick={() => {
-    //       setIsToggled(!isToggled);
-    //     }}
-    //   >
-    //     <FontAwesomeIcon icon={!isToggled ? faBars : faTimes} />
-    //   </div>
+    <Header isToggled={isToggled} userToggled={userToggled}>
+      {/* 햄버거 버튼(bar) */}
+      <div
+        className="toggle"
+        onClick={() => {
+          setIsToggled(!isToggled);
+        }}
+      >
+        <FontAwesomeIcon icon={!isToggled ? faBars : faTimes} />
+      </div>
 
-    //   {/* 로고 */}
-    //   <div className="logo">
-    //     <FontAwesomeIcon icon={faHeart} />
-    //   </div>
+      {/* 로고 */}
+      <div className="logo">
+        <FontAwesomeIcon icon={faHeart} />
+      </div>
 
-    //   {/* User 버튼 */}
-    //   <div
-    //     className="user"
-    //     onClick={() => {
-    //       setUserToggled(!userToggled);
-    //     }}
-    //   >
-    //     <FontAwesomeIcon icon={!userToggled ? faUser : faTimes} />
-    //   </div>
+      {/* User 버튼 */}
+      <div
+        className="user"
+        onClick={() => {
+          setUserToggled(!userToggled);
+        }}
+      >
+        <FontAwesomeIcon icon={!userToggled ? faUser : faTimes} />
+      </div>
 
       
-    //   {/* 메뉴 리스트 */}
-    //   <ul className="header__menulist">
-    //     <li>테스트</li>
-    //     <li>정보</li>
-    //     <li>마이페이지</li>
-    //   </ul>
+      {/* 메뉴 리스트 */}
+      <ul className="header__menulist">
+        <li>테스트</li>
+        <li>정보</li>
+        <li>마이페이지</li>
+      </ul>
 
-    //   {/* User 메뉴 리스트 */}
-    //   <ul className="header__right">
-    //     <li>로그인</li>
-    //     <li>회원가입</li>
-    //   </ul>
-    // </Header>
+      {/* User 메뉴 리스트 */}
+      <ul className="header__right">
+        <li>로그인</li>
+        <li>회원가입</li>
+      </ul>
+    </Header>
 
     
   );
